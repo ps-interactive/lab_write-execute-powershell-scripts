41c0e9da8a1a25176a6ea92b427892c3e6367f3b
SharePlicity <liamcleary@shareplicity.com>
2023-08-15 17:26:58 -0400

    Updated LAB files, included Sample Data

--- Creating-PowerShell-Scripts.ps1
+++ Creating-PowerShell-Scripts.ps1
@@ -1,1 +1,145 @@
+## Create a Basic Function with Simple Output
+function Show-Message {
+    Write-Output "Hello, World!"
+}
+
+Show-Message
+
+
+## Add Parameters to the Function
+function Show-Message {
+    param(
+        [string]$Name
+    )
+    Write-Output "Hello, $Name!"
+}
+
+Show-Message -Name "Alice"
+
+
+## Add Default Parameter Value and Multiple Parameters
+function Show-Message {
+    param(
+        [string]$Name = "World",
+        [string]$Greeting = "Hello"
+    )
+    Write-Output "$Greeting, $Name!"
+}
+
+Show-Message
+Show-Message -Name "Dave"
+Show-Message -Name "Lucy" -Greeting "Hi"
+
+
+## Add Conditional Logic
+function Show-Message {
+    param(
+        [string]$Name = "World",
+        [string]$Greeting
+    )
+    
+    if (-not $Greeting) {
+        $currentHour = (Get-Date).Hour
+        $Greeting = if ($currentHour -lt 12) {
+            "Good morning"
+        } elseif ($currentHour -lt 18) {
+            "Good afternoon"
+        } else {
+            "Good evening"
+        }
+    }
+
+    Write-Output "$Greeting, $Name!"
+}
+
+Show-Message
+Show-Message -Name "Lisa"
+Show-Message -Name "Liam" -Greeting "Hi"
+
+## Retrieve Basic System Information
+Get-CimInstance -ClassName Win32_OperatingSystem
+
+## Filter the Data
+Get-CimInstance -ClassName Win32_DiskDrive -Filter "MediaType='Fixed hard disk media'"
+
+## Use Where-Object for Advanced Filtering
+Get-CimInstance -ClassName Win32_LogicalDisk | Where-Object { $_.Size -gt 100GB }
+
+## Use ForEach-Object to Manipulate or Act on Each Instance
+Get-CimInstance -ClassName Win32_Service | ForEach-Object {
+    [PSCustomObject]@{
+        'Service Name'  = $_.Name
+        'Display Name'  = $_.DisplayName
+        'Status'        = $_.Status
+        'Start Mode'    = $_.StartMode
+    }
+} | Format-Table -AutoSize
+
+
+## Combining CIM Classes
+$topProcess = Get-CimInstance -ClassName Win32_PerfFormattedData_PerfProc_Process | 
+              Sort-Object WorkingSet -Descending | 
+              Select-Object -First 1
+
+$processDetail = Get-CimInstance -ClassName Win32_Process | 
+                 Where-Object { $_.ProcessId -eq $topProcess.IDProcess }
+
+[PSCustomObject]@{
+    'Process Name' = $processDetail.Name
+    'Memory (MB)'  = [math]::Round($topProcess.WorkingSet / 1MB, 2)
+}
+
+
+## Retrieve System Information
+function Get-SystemInformation {
+    $OS = Get-CimInstance Win32_OperatingSystem
+    $Processor = Get-CimInstance Win32_Processor
+    $Memory = Get-CimInstance Win32_PhysicalMemory
+
+    $MemoryInfo = ($Memory | ForEach-Object {
+        "Size: $([math]::Round($_.Capacity / 1GB, 2)) GB, Manufacturer: $($_.Manufacturer), Speed: $($_.Speed) MHz"
+    }) -join '; '
+
+    # Output the retrieved information
+    [PSCustomObject]@{
+        'OS Name' = $OS.Caption
+        'OS Version' = $OS.Version
+        'Processor Name' = $Processor.Name
+        'Total Physical Memory (GB)' = [math]::Round($OS.TotalVisibleMemorySize / 1MB, 2)
+        'Free Physical Memory (GB)' = [math]::Round($OS.FreePhysicalMemory / 1MB, 2)
+        'Memory Modules' = $MemoryInfo
+    }
+}
+
+# Call the Function
+Get-SystemInformation | Format-Table -AutoSize
+
+
+
+## Import CSV Data and Modify
+function Invoke-DataChange {
+    param(
+        [string]$FilePath
+    )
+
+    $data = Import-Csv -Path $FilePath
+
+    $updatedData = $data | ForEach-Object {
+        [PSCustomObject]@{
+            'ID'        = $_.ID
+            'Name'      = $_.Name
+            'Age'       = "$($_.Age) years old"
+            'Company'   = $_.Company
+            'Department'= $_.Department
+            'City'      = $_.City
+            'State'     = $_.State
+            'Country'   = $_.Country
+            'Status'    = if ($_.Age -lt 30) { "Young" } else { "Experienced" }
+        }
+    }
+    $updatedData
+}
+
+# Call the function
+Invoke-DataChange -FilePath ".\Data\DataUpdate.csv" | Format-Table -AutoSize
 
--- Manipulating-Data-using-PowerShell.ps1
+++ Manipulating-Data-using-PowerShell.ps1
@@ -1,1 +1,153 @@
+# Modify Previously Created Variable
+$stringVariable = "Hello, Learner!"
+$additionalStringVariable = ", Welcome to the LAB"
+$stringVariable += $additionalStringVariable
+Write-Host "String Variable: $stringVariable"
+
+
+## Modify Previously Created Hashtable Variables
+# Add a New Column
+$hashtable["Occupation"] = "Engineer"
+$hashtable
+
+# Update an Existing Key Value
+$hashtable["Age"] = "31"
+$hashtable
+
+# Add a New Column to a Nested Hashtable
+foreach ($key in $employeesWithIdentifiers.Keys) {
+    $employeesWithIdentifiers[$key]["Country"] = "USA"
+}
+
+$employeesWithIdentifiers
+
+# Add a New Row toi the Nested Hashtable
+$employeesWithIdentifiers["EID47389"] = @{
+    "Name"       = "Alice Brown"
+    "Age"        = 32
+    "Department" = "Human Resources"
+    "Country" = "UK"
+}
+
+$employeesWithIdentifiers
+
+
+## Create Variables Containing Data
+# Create a JSON Variable Loaded Static with Data
+$jsonData = @"
+{
+    "users": [
+        {"id": 1, "name": "Alice"},
+        {"id": 2, "name": "Bob"},
+        {"id": 3, "name": "Charlie"},
+        {"id": 4, "name": "Dave"},
+        {"id": 5, "name": "Eve"},
+        {"id": 6, "name": "Frank"},
+        {"id": 7, "name": "Grace"},
+        {"id": 8, "name": "Hannah"},
+        {"id": 9, "name": "Isaac"},
+        {"id": 10, "name": "Jack"}
+    ]
+}
+"@
+
+# Create a XML Variable Loaded Static with Data
+$xmlData = @"
+<users>
+    <user id="1"><name>Alice</name></user>
+    <user id="2"><name>Bob</name></user>
+    <user id="3"><name>Charlie</name></user>
+    <user id="4"><name>Dave</name></user>
+    <user id="5"><name>Eve</name></user>
+    <user id="6"><name>Frank</name></user>
+    <user id="7"><name>Grace</name></user>
+    <user id="8"><name>Hannah</name></user>
+    <user id="9"><name>Isaac</name></user>
+    <user id="10"><name>Jack</name></user>
+</users>
+
+"@
+
+# Create a CSV Variable Loaded Static with Data
+$csvData = @"
+    id,name
+    1,Alice
+    2,Bob
+    3,Charlie
+    4,Dave
+    5,Eve
+    6,Frank
+    7,Grace
+    8,Hannah
+    9,Isaac
+    10,Jack
+"@
+
+$jsonData
+$xmlData
+$csvData
+
+
+
+# Create a JSON Variable with Imported Data
+$jsonImportPath = ".\Data\Data.json"
+$jsonImportData = Get-Content -Path $jsonImportPath -Raw | ConvertFrom-Json
+
+# Create a XML Variable with Imported Data
+$xmlImportPath = ".\Data\Data.xml"
+$xmlImportData = [xml](Get-Content -Path $xmlImportPath)
+
+# Create a CSV Variable with Imported Data
+$csvImportPath = ".\Data\Data.csv"
+$csvImportData = Import-Csv -Path $csvImportPath
+
+$jsonImportData
+$xmlImportData
+$csvImportData
+
+
+
+# Loop the JSON Data in both Variables Using ForEach-Object
+$jsonData.users | ForEach-Object {
+    Write-Host "ID: $($_.id), Name: $($_.name)"
+}
+
+$jsonImportData.users | ForEach-Object {
+    Write-Host "ID: $($_.id), Name: $($_.name)"
+}
+
+# Loop the JSON Data in both Variables Using Foreach
+foreach ($user in $jsonData.users) {
+    Write-Host "ID: $($user.id), Name: $($user.name)"
+}
+
+foreach ($user in $jsonImportData.users) {
+    Write-Host "ID: $($user.id), Name: $($user.name)"
+}
+
+
+# Modify the XML Loaded Data and Update
+$xmlImportPath = ".\Data\Data.xml"
+$xmlImportData = [xml](Get-Content -Path $xmlImportPath)
+
+# Loop Through and View the Data
+foreach ($employee in $xmlImportData.employees.employee) {
+    Write-Host "ID: $($employee.id), Name: $($employee.name), Country: $($employee.country)" 
+}
+
+# Loop Through and Update the Data
+foreach ($employee in $xmlImportData.employees.employee) {
+    $employee.country = "USA"
+}
+
+# Save the Modified XML back to the File
+$xmlImportData.Save($xmlImportPath)
+
+# Display the Modified XML Content
+$xmlImportData.OuterXml
+
+
+
+
+
 
